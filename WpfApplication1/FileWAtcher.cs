65a09a270ceac482084b23418c4e12538cb5ea33
MaciejTroszyński <mt@omni3d.net>
2016-09-12 13:55:32 +0200

    Add project files.

---
+++ WpfApplication1/FileWAtcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class FileWatcher : FileSystemWatcher
+    {
+        public FileWatcher()
+        {
+            this.Path = "C:/Users/maciejt/Pictures";
+            this.Filter = "*.jpg";
+            this.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+           | NotifyFilters.FileName | NotifyFilters.DirectoryName; 
+            
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        public MainWindow()
+        {
+            InitializeComponent();
+            watcher.Path = "C:/Users/maciejt/Pictures";
+            watcher.Filter = "*.jpg";
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.EnableRaisingEvents = true;
+
+            image.Source = LoadBitmapImage("C:/Users/maciejt/Pictures/1.jpg");
+        }
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);            
+            image.Dispatcher.Invoke(new Action(() => { image.Source = LoadBitmapImage(e.FullPath); }));
+        }
+
+        public static BitmapImage LoadBitmapImage(string fileName)
+        {
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = stream;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+                return bitmapImage;
+            }
+        }
+
+    }
+
+    
+
+}
